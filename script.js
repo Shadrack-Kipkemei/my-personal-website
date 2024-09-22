3f0319f5132afa7ed83801b1ee6aa814768dfac9
sktanui <shadrackkemei2019@gmail.com>
2024-09-23 01:00:02 +0300

    index.html

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+let menuIcon = document.querySelector("#menu-icon");
+let topnav = document.querySelector(".topnav");
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    topnav.classLight.toggle("active")
+}
