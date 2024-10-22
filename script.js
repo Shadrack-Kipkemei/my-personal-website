51ffd43b7ef25309edb6038f3bdc9a556f5b1f9d
sktanui <shadrackkemei2019@gmail.com>
2024-10-22 17:00:16 +0300

    first commit

--- script.js
+++
@@ -1,6 +0,0 @@
-let menuIcon = document.querySelector("#menu-icon");
-let topnav = document.querySelector(".topnav");
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle("bx-x");
-    topnav.classLight.toggle("active")
-}
